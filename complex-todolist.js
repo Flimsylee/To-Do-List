5754c7d8afbf28f7c5c0646e5bef8471b76278d7
Mumeen Olamilekan Abass <38820501+Flimsylee@users.noreply.github.com>
2023-08-15 16:58:07 -0700

    Update complex-todolist.js
    JavaScript code that portray the functionalities of to-do list.

--- complex-todolist.js
+++ complex-todolist.js
@@ -15,15 +15,52 @@ li.innerText = `${inputTask.value}`;
 task.appendChild(li);
 
 let checkButton = document.createElement('button');
-checkButton.innerHTML = `<i class="fa-solid fa-check"></i>`;
-checkButton.classList.add('checkTask');
+
+checkButton.style.border="2px solid orange";
+let checkTask = document.createElement("i");
+checkTask.classList.add('fa-solid');
+checkTask.classList.add('fa-check');
+checkButton.style.color="orange";
+checkButton.style.fontSize="1.3rem";
+checkButton.style.marginRight="5px";
+checkButton.appendChild(checkTask);
 task.appendChild(checkButton);
+
+checkButton.addEventListener("mouseover", function(){
+checkButton.style.backgroundColor = "orange";
+checkTask.style.color="white";
+})
+
+checkButton.addEventListener("mouseout", function(){
+checkButton.style.backgroundColor = "#fff";
+checkTask.style.color = "";
+})
 
 
 let deleteButton = document.createElement('button');
-deleteButton.innerHTML = `<i class="fa-solid fa-trash-can"></i>`;
-task.classList.add('deleteTask');
+
+deleteButton.style.border="2px solid red";
+let deleteTask = document.createElement("i");
+deleteTask.classList.add('fa-solid');
+deleteTask.classList.add('fa-trash-can');
+deleteButton.style.color="red";
+deleteButton.style.fontSize="1.3rem";
+
+deleteButton.appendChild(deleteTask)
 task.appendChild(deleteButton);
+
+
+
+deleteButton.addEventListener("mouseover", function(){
+deleteButton.style.backgroundColor = "red";
+deleteTask.style.color="white";
+})
+
+deleteButton.addEventListener("mouseout", function(){
+deleteButton.style.backgroundColor = "#fff";
+deleteTask.style.color = "";
+})
+
 
 if (inputTask.value === "") {
     alert('Please Enter a task');
@@ -31,5 +68,18 @@ if (inputTask.value === "") {
     taskContainer.appendChild(task);
 }
 
+inputTask.value ="";
 
+
+ checkButton.addEventListener('click', function(){
+ 
+ checkButton.parentElement.style.textDecoration="line-through";
+ 
+ });
+ 
+ deleteButton.addEventListener('click', function(e){
+ 
+ let target = e.target;
+ target.parentElement.parentElement.remove();
+ });
 });
